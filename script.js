e428d24c337aafd8d9d553750d97ffbf01b76ab3
Your Name <your.email@example.com>
2025-09-11 20:00:48 +0300

    hello

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
+      let current = 0;
+
       // creating nav 
       const navBar = document.createElement('nav');
       navBar.className = 'navBar';
@@ -63,42 +65,55 @@ document.addEventListener('DOMContentLoaded', function () {
       createLogin();
 
       function createContentContainer() {
+            // create main container
             const contentContainer = document.createElement('main');
             contentContainer.className = 'contentContainer';
             contentContainer.id = 'contentContainer';
             document.body.appendChild(contentContainer);
 
+            // create first section container
             const internalContainer = document.createElement('div');
             internalContainer.className = 'internalContainer';
             internalContainer.id = 'cardsContainer';
             contentContainer.appendChild(internalContainer);
 
+            //create cards container
             const bigCArd = document.createElement('div');
             bigCArd.id = 'bigCArd';
             bigCArd.classList.add('bigCArd')
             internalContainer.appendChild(bigCArd);
+
+            // create div for first elements
+            const article = document.createElement('div');
+            article.id = 'artical';
+            article.className = 'artical';
+            bigCArd.appendChild(article);
             // create  h1
             const hOne = document.createElement('h1');
             hOne.textContent = 'New Features';
-            bigCArd.appendChild(hOne)
+            article.appendChild(hOne)
+
             // create pragraph 
             const pragraph = document.createElement('p');
             pragraph.textContent = 'Explor our new items and see what is new'
-            bigCArd.appendChild(pragraph);
+            article.appendChild(pragraph);
+
             // create ul 
             function crateMenu() {
                   const menuContain = document.createElement('ul');
-                  bigCArd.appendChild(menuContain);
+                  article.appendChild(menuContain);
 
                   const listData = [
                         { text: 'reducing wrinkles and skin discolorations.' },
                         { text: 'based retinol alternative with antioxidant.' },
                         { text: 'Promote healthy toe splay and alignment.' },
                         { text: 'relieving foot pain and improving balance.' },
+                        { text: 'View our new AI agent' },
+                        { text: 'contact us for more deteals' }
                   ]
                   // console.log(listData)
-                  listData.forEach(function(item, index){
-                        const strong = document.createElement('strong')
+                  listData.forEach(function (item, index) {
+                        const strong = document.createElement('p')
                         strong.textContent = item.text
                         const list = document.createElement('li')
                         list.classList.add('list')
@@ -107,8 +122,74 @@ document.addEventListener('DOMContentLoaded', function () {
                   })
             }
             crateMenu()
+            // create slides
+            function slides() {
+                  const bigCArd = document.getElementById('bigCArd');
+                  // create slide container 
+                  const slide = document.createElement('div');
+                  slide.id = 'slide';
+                  slide.className = 'slide';
+                  bigCArd.appendChild(slide);
 
+                  const imgData = [
+                        { src: 'img/Dessert.jpeg', title: 'card img', alt: 'card img' },
+                        { src: 'img/card-2.jpg', title: 'card img', alt: 'card img' },
+                        { src: 'img/pasta.jpg', title: 'card img', alt: 'card img' },
+                        { src: 'img/card-one.jpg', title: 'card img', alt: 'card img' }
+                  ]
+                  // forEach loop 
+                  imgData.forEach(function (item, index) {
+                        const imgDiv = document.createElement('div');
+                        imgDiv.id = 'imgDiv';
+                        imgDiv.className = 'imgDiv';
+                        slide.appendChild(imgDiv);
+                        const img = document.createElement('img');
+                        img.src = item.src;
+                        img.title = item.title;
+                        img.alt = item.alt;
+                        img.className = 'img'
+                        imgDiv.appendChild(img);
+                  })
+
+                  // create last part in the card
+                  const linkGo = document.createElement('div');
+                  linkGo.id = 'linkGo';
+                  linkGo.className = 'linkGo';
+                  bigCArd.appendChild(linkGo);
+                  // console.log(linkGo)
+                  // create p 
+                  const viewLink = document.createElement('a');
+                  viewLink.href = '#view'
+                  viewLink.textContent = 'View more  >'
+                  // console.log(viewLink)
+
+                  linkGo.appendChild(viewLink);
+                  // console.log(span)
+            }
+            slides();
       }
       createContentContainer()
 
+      // create slide function 
+      function slideShow() {
+            const imgDiv = document.querySelectorAll('#imgDiv');
+            if (current >= imgDiv.length) { current = 0 };
+            if (current < 0) { current = imgDiv.length - 1 };
+            console.log(imgDiv)
+            imgDiv.forEach((item, index) => {
+                  item.style.display = index === current ? 'block' : 'none';
+            })
+      }
+      
+      slideShow();
+
+      // counter
+      // const imgDiv1 = document.getElementById('imgDiv-1');
+      // imgDiv1.style.display = 'block';
+      // const imgDiv2 = document.getElementById('imgDiv-2');
+      // imgDiv2.style.display = 'none';
+      // const imgDiv3 = document.getElementById('imgDiv-3');
+      // imgDiv3.style.display = 'none';
+      // const imgDiv4 = document.getElementById('imgDiv-4');
+      // imgDiv4.style.display = 'none';
 });
